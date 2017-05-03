889fbaa403a0ef3124d79884552a9728aa3ab665
aharis <ammar.haris@gmail.com>
2017-05-02 22:50:16 -0700

    Deploy

--- go/package.go
+++ go/package.go
@@ -11,6 +11,6 @@ type Package struct {
 func GetInventory(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("500 - Something bad happened!"))
+		w.Write([]byte("Foo bar! - Something Strange happed. Dont know what :/ !"))
 }
 
