8449f103168fd3b817f50547b439a53daf831749
aharis <ammar.haris@gmail.com>
2017-05-04 00:19:42 -0700

    Deploy

--- go/routers.go
+++ go/routers.go
@@ -17,7 +17,9 @@ type Routes []Route
 
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
+	router.HandleFunc("/", Index)
+
+	/*for _, route := range routes {
 		var handler http.Handler
 		handler = route.HandlerFunc
 		handler = Logger(handler, route.Name)
@@ -27,7 +29,7 @@ func NewRouter() *mux.Router {
 			Path(route.Pattern).
 			Name(route.Name).
 			Handler(handler)
-	}
+	}*/
 
 	return router
 }
