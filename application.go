e9803d725363442970740ce738415e882148afcb
aharis <ammar.haris@gmail.com>
2017-05-03 23:59:30 -0700

    Deploy

--- application.go
+++ application.go
@@ -8,15 +8,51 @@ import (
 	//
 	//    sw "github.com/myname/myrepo/go"
 	//
-	sw "./go"
+	//sw "./go"
 	"log"
+	//"net/http"
+
+	"os"
 	"net/http"
+	"io/ioutil"
 )
 
 func main() {
+
 	log.Printf("Server started")
 
-	router := sw.NewRouter()
+//	router := sw.NewRouter()
 	
-	log.Fatal(http.ListenAndServe(":5000", router))
+//	log.Fatal(http.ListenAndServe(":5000", router))
+
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+
+	f, _ := os.Create("/var/log/golang/golang-server.log")
+	defer f.Close()
+	log.SetOutput(f)
+
+	const indexPage = "public/index.html"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			if buf, err := ioutil.ReadAll(r.Body); err == nil {
+				log.Printf("Received message: %s\n", string(buf))
+			}
+		} else {
+			log.Printf("Serving %s to %s...\n", indexPage, r.RemoteAddr)
+			http.ServeFile(w, r, indexPage)
+		}
+	})
+
+	http.HandleFunc("/scheduled", func(w http.ResponseWriter, r *http.Request){
+		if r.Method == "POST" {
+			log.Printf("Received task %s scheduled at %s\n", r.Header.Get("X-Aws-Sqsd-Taskname"), r.Header.Get("X-Aws-Sqsd-Scheduled-At"))
+		}
+	})
+
+	log.Printf("Listening on port %s\n\n", port)
+	http.ListenAndServe(":"+port, nil)
 }
