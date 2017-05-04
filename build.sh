ea3847942c0ca78b95a5bc7fed848cd84a5d05f7
aharis <ammar.haris@gmail.com>
2017-05-04 10:45:33 -0700

    Deploy

--- build.sh
+++ build.sh
@@ -37,6 +37,7 @@ echo "Hello World Path!"
 #go get -u gopkg.in/yaml.v2
 go get -u github.com/gorilla/mux
 echo "Hello World Post MUX!"
+go build -o bin/application application.go
 
 # Build the server. Beanstalk will look for an 'application' executable.
 #cd $DEST
